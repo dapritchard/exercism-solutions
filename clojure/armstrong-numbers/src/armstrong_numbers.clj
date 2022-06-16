3b7bb7d56f1e6a4b84fc33b801df71b2cc4ed9b8
David Pritchard <david.al.pritchard@gmail.com>
2022-06-16 09:22:53 -0400

    update clojure/armstrong-numbers

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -7,11 +7,18 @@
              (cons (rem num 10)
                    acc))))
 
+(defn pow [num expt]
+  (defn pow-impl [sum expt]
+    (if (<= expt 0)
+      sum
+      (pow-impl (* sum num) (- expt 1))))
+  (pow-impl 1N expt))
+
 (defn calc-armstrong [num]
   (let [digits-list (to-list num '())
         len (count digits-list)]
-    (long (reduce #(+ (Math/pow %2 len) %1)
-                  0
+    (long (reduce #(+' (pow %2 len) %1)
+                  0N
                   digits-list))))
 
 (defn armstrong? [num]
