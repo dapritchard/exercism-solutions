88d4a47a63a66fe66a769812f3cea3fda5dfd185
David Pritchard <david.al.pritchard@gmail.com>
2022-06-25 11:32:54 -0400

    working version of clojure/nth-prime

--- clojure/nth-prime/src/nth_prime.clj
+++ clojure/nth-prime/src/nth_prime.clj
@@ -8,7 +8,7 @@
         true
         (if (zero? (rem n m))
           false
-          (check-odds (+ m 2)))))
+          (recur (+ m 2)))))
     (cond
       (= n 2) true
       (zero? (rem n 2)) false
@@ -18,18 +18,18 @@
   "Finds the next prime number >= `m`. Assumes that `m` is odd."
   (if (check-prime m)
     m
-    (find-next-prime (+ m 2))))
+    (recur (+ m 2))))
 
 (defn nth-prime [n]
   "Returns the prime number in the nth position."
   (defn nth-prime-impl [m p]
     (if (>= p n)
       m
-      (nth-prime-impl (find-next-prime (+ m 2)) (+ p 1))))
+      (recur (find-next-prime (+ m 2)) (+ p 1))))
   (cond
-    (or (not (integer? n)) (<= n 0))
+    (or (not (integer? n)) (< n 1))
     (throw (IllegalArgumentException.
-            "The input must be an integer no less than 0 and less than 1000000000000"))
+            "The input must be an integer no less than 1"))
     (= n 1) 2
     (= n 2) 3
     :else (nth-prime-impl 3 2)))
