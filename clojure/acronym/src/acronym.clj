a4cf90a9a48dc5cb02de0594e8b7652e61cef4e5
David Pritchard <david.al.pritchard@gmail.com>
2022-06-29 23:13:38 -0400

    add clojure/acronym

--- clojure/acronym/src/acronym.clj
+++ clojure/acronym/src/acronym.clj
@@ -1,16 +1,20 @@
-(ns acronym)
+(ns acronym
+  (:require [clojure.string :as string]))
 
 (defn acronym
   "Converts phrase to its acronym."
   [phrase]
+  (defn split-subwords-rest [word]
+    (let [next-upper (string/replace word #"^[\p{Upper}]*[^\p{Upper}]*" "")]
+      (if (empty? next-upper)
+        ()
+        (cons (subs next-upper 0 1) (split-subwords-rest next-upper)))))
   (defn split-subwords [word]
     (if (empty? word)
       ()
-      (let [first (subs word 0 1)
-            next-upper (clojure.string/replace word #"^.[^\p{Upper}]*" "")]
-        (cons first (split-subwords next-upper)))))
-  (let [words (clojure.string/split phrase #"[- ]")
+      (cons (subs word 0 1) (split-subwords-rest (subs word 1)))))
+  (let [words (string/split phrase #"[- ]")
         subwords-depth-2 (map split-subwords words)
         subwords-depth-1 (apply concat subwords-depth-2)
-        subwords-str (clojure.string/join subwords-depth-1)]
-    (clojure.string/upper-case subwords-str)))
+        subwords-str (string/join subwords-depth-1)]
+    (string/upper-case subwords-str)))
